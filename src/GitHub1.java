d4be6c337ad69c37b558d6620f65b7d9023eb387
Krishna Kharel <krishnarheakharel@gmail.com>
2023-09-19 19:07:53 +0100

    My first commit

---
+++ src/GitHub1.java
@@ -0,0 +1,37 @@
+public class GitHub1
+{
+static String string1,string2;
+
+
+
+//static GitHub1 gitHub1= new GitHub1();
+
+
+
+
+
+public static void main (String[]args)
+{
+   // gitHub1.joinStrings();
+joinStrings();
+
+}
+
+
+public static void joinStrings()
+{
+    string1="PHP Exercises and";
+    string2 ="Python Exercises";
+
+    System.out.println("The concatenated string: " +string1 +" " +string2);
+}
+
+
+
+
+
+
+
+
+
+}
